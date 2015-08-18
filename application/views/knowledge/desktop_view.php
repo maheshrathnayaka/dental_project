5cdeb68059127201e408479eb858b60ec2971366
Mahesh Rathnayaka <mintcrops@gmail.com>
2015-08-18 19:13:32 +0530

    Knowledge-base related modifications - Desktop application path

--- application/views/knowledge/desktop_view.php
+++ application/views/knowledge/desktop_view.php
@@ -35,60 +35,6 @@
                                 <!-- The GRID System -->
                                 <div class="metro-gal-container noborder norounded dark-bg-entries">
 
-                                    <!-- A GALLERY ENTRY -->
-<!--                                    <div class="mega-entry cat-two cat-all" id="mega-entry-1" data-src="--><?php //echo base_url(); ?><!--tempImg/vre.bmp" data-width="780" data-height="585" data-lowsize="">-->
-<!--                                        <div class="mega-covercaption mega-square-bottom mega-landscape-right mega-portrait-bottom mega-red">-->
-<!--                                        </div>-->
-<!--                                        <div class="pull-right">-->
-<!--                                            <input id="testInput" type="checkbox" style="width:50px; height:50px;">-->
-<!--                                        </div>-->
-<!--                                        <!-- The Link Buttons -->
-<!--                                        <div class="mega-coverbuttons">-->
-<!--                                            <a class="fancybox" rel="group" href="--><?php //echo base_url(); ?><!--tempImg/vre.bmp" title="--><?php //echo base_url(); ?><!--tempImg/vre.bmp"><div class="mega-view mega-red"></div></a>-->
-<!--                                        </div>-->
-<!--                                    </div>-->
-
-                                    <!-- A GALLERY ENTRY -->
-<!--                                    <div class="mega-entry cat-two cat-all" id="mega-entry-1" data-src="--><?php //echo base_url(); ?><!--tempImg/vre.bmp" data-width="780" data-height="585" data-lowsize="">-->
-<!--                                        <div class="mega-covercaption mega-square-bottom mega-landscape-right mega-portrait-bottom mega-red">-->
-<!--                                        </div>-->
-<!--                                        <div class="pull-right">-->
-<!--                                            <input id="testInput" type="checkbox" style="width:50px; height:50px;">-->
-<!--                                        </div>-->
-<!--                                        <!-- The Link Buttons -->
-<!--                                        <div class="mega-coverbuttons">-->
-<!--                                            <a class="fancybox" rel="group" href="--><?php //echo base_url(); ?><!--tempImg/vre.bmp" title="--><?php //echo base_url(); ?><!--tempImg/vre.bmp"><div class="mega-view mega-red"></div></a>-->
-<!--                                        </div>-->
-<!--                                    </div>-->
-
-                                    <!-- A GALLERY ENTRY -->
-<!--                                    <div class="mega-entry cat-two cat-all" id="mega-entry-1" data-src="--><?php //echo base_url(); ?><!--tempImg/vre.bmp" data-width="780" data-height="585" data-lowsize="">-->
-<!--                                        <div class="mega-covercaption mega-square-bottom mega-landscape-right mega-portrait-bottom mega-red">-->
-<!--                                        </div>-->
-<!--                                        <div class="pull-right">-->
-<!--                                            <input id="testInput" type="checkbox" style="width:50px; height:50px;">-->
-<!--                                        </div>-->
-<!--                                        <!-- The Link Buttons -->
-<!--                                        <div class="mega-coverbuttons">-->
-<!--                                            <a class="fancybox" rel="group" href="--><?php //echo base_url(); ?><!--tempImg/vre.bmp" title="--><?php //echo base_url(); ?><!--tempImg/vre.bmp"><div class="mega-view mega-red"></div></a>-->
-<!--                                        </div>-->
-<!--                                    </div>-->
-
-                                    <!-- A GALLERY ENTRY -->
-<!--                                    <div class="mega-entry cat-two cat-all" id="mega-entry-1" data-src="--><?php //echo base_url(); ?><!--tempImg/vre.bmp" data-width="780" data-height="585" data-lowsize="">-->
-<!--                                        <div class="mega-covercaption mega-square-bottom mega-landscape-right mega-portrait-bottom mega-red">-->
-<!--                                        </div>-->
-<!--                                        <div class="pull-right">-->
-<!--                                            <input id="testInput" type="checkbox" style="width:50px; height:50px;">-->
-<!--                                        </div>-->
-<!--                                        <!-- The Link Buttons -->
-<!--                                        <div class="mega-coverbuttons">-->
-<!--                                            <a class="fancybox" rel="group" href="--><?php //echo base_url(); ?><!--tempImg/vre.bmp" title="--><?php //echo base_url(); ?><!--tempImg/vre.bmp"><div class="mega-view mega-red"></div></a>-->
-<!--                                        </div>-->
-<!--                                    </div>-->
-
-
-
                                     <?php
                                     $imgdir = 'tempImg/'; //Pick your folder
                                     $allowed_types = array('png', 'jpg', 'jpeg', 'bmp'); //Allowed types of files
@@ -99,52 +45,31 @@
                                             /*If the file is an image add it to the array*/
                                         {$a_img[] = $imgfile;}
                                     }
-                                    echo "<ul>";
                                     $totimg = count($a_img);  //The total count of all the images
                                     //Echo out the images and their paths incased in an li.
                                     for($x=0; $x < $totimg; $x++){
-                                        echo '<li><img src="'. base_url().$imgdir . $a_img[$x].'" /></li>';
-                                        echo base_url().$imgdir . $a_img[$x];
+                                        ?>
+                                        <div class="mega-entry cat-two cat-all" id="mega-entry-1" data-src="<?php echo base_url().$imgdir . $a_img[$x]; ?>" data-width="780" data-height="585" data-lowsize="">
+                                            <div class="mega-covercaption mega-square-bottom mega-landscape-right mega-portrait-bottom mega-red">
+                                            </div>
+                                            <div class="pull-right">
+                                                <input id="chkDesktopImages" name="chkDesktopImages" value="<?php echo base_url().$imgdir . $a_img[$x]; ?>" type="checkbox" style="width:50px; height:50px;">
+                                            </div>
+                                            <!-- The Link Buttons -->
+                                            <div class="mega-coverbuttons">
+                                                <a class="fancybox" rel="group" href="<?php echo base_url().$imgdir . $a_img[$x]; ?>" title="<?php echo $a_img[$x]; ?>"><div class="mega-view mega-red"></div></a>
+                                            </div>
+                                        </div>
+                                    <?php
                                     }
-                                    echo "<li><img src='http://localhost/dental_project/tempImg/vre.bmp' /></li>";
-                                    echo "</ul>";
                                     ?>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
                                 </div>
+                            </div>
+                            <div class="form-actions text-right">
+                                <button id="btnDesktopImageSelectProceed" name="btnDesktopImageSelectProceed" type="button" class="btn btn-large btn-success">
+                                    <i class="icon-ok"></i> Proceed
+                                </button>
+                                <a href="<?php echo base_url(); ?>index.php/knowledge" class="btn btn-large btn-success"><i class=" icon-arrow-left"></i> Back</a>
                             </div>
                         </div>
                     </div>
