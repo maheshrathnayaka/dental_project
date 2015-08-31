0a72a5a2f2ed90ce7cfe6b8f851296424fe8d702
Mahesh Rathnayaka <mintcrops@gmail.com>
2015-08-31 18:48:16 +0530

    File moving related changes

--- application/controllers/Knowledge.php
+++ application/controllers/Knowledge.php
@@ -9,9 +9,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Knowledge extends CI_Controller
 {
+    public function __construct() {
+        parent::__construct();
+        $this->load->helper('url');
+        $this->load->helper('file');
+    }
 
     public function index()
     {
+//        var_dump($_SESSION['user_id']);
         $dynamic_data = array(
             'title' => 'Knowledge Base'
         );
@@ -48,6 +54,27 @@ class Knowledge extends CI_Controller
         $this->load->view('common/header', $dynamic_data);
         $this->load->view('common/sidebar');
         $this->load->view('knowledge/doctor_view');
+        $this->load->view('common/footer');
+    }
+
+    public function add_comments(){
+        $DesktopImages = $_POST['chkDesktopImages'];
+        if(!empty($DesktopImages)) {
+            $N = count($DesktopImages);
+        }
+        foreach($DesktopImages as $key => $value){
+//            $newfile = explode('/', $value);
+//            write_file(base_url().'uploads/'.$newfile[count($newfile)-1] , $value, 'r+');
+//            copy($value, base_url().'uploads/'.$newfile[count($newfile)-1]);
+        }
+        $dynamic_data = array(
+            'title' => 'Knowledge Base - Add Comments',
+            'imgs' => $DesktopImages,
+            'img_count' => $N
+        );
+        $this->load->view('common/header', $dynamic_data);
+        $this->load->view('common/sidebar');
+        $this->load->view('knowledge/comment_view');
         $this->load->view('common/footer');
     }
 
